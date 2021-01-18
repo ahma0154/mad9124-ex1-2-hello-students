05c52b2d48b09fdca87b382d51f9c590027e837f
Saif Khalid <ahma0154@algonquinlive.com>
2021-01-17 21:01:30 -0500

    modified version

--- helloStudents.js
+++ helloStudents.js
@@ -2,10 +2,28 @@
 
 // 1. Read the JSON file into a variable called students
 
+let students = require('./students.json')
+console.log(students);
+
 // 2. Iterate over the students array and print Hello with their full names to the console
 // e.g. Hello Walter Baker
 
+students.forEach(function(student) {
+    console.log(`Hello ${student.firstName} ${student.lastName}`);
+    
+})
+
+console.log('\n');
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
 
-//test
+console.log(students.filter(s=>{
+    return s.lastName.charAt(0)==='D';
+}).length);
+
+console.log(students.filter(s=>{
+    return s.lastName.charAt(0)==='D';
+}).map(s=>s.firstName+' '+s.lastName).join('\n'));
+
+
+
